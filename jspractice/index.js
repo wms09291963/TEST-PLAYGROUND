2e88f5fff953e6662103aecf34c710f63d1cc66a
Smith <william.smith@bankerslife.com>
2022-07-23 16:35:38 -0400

    Adding home page view

---
+++ jspractice/index.js
@@ -0,0 +1,21 @@
+// write a function to count all the vowels of a given string
+// vowels are a,e,i,o,u
+//{a:3, e:2, 1:2, o:7, u:1}
+[2,4,5].includes(8)
+
+const testStr = "elirajlrudafidaeeafier3wqirja;lrkeaj;iweqooerodfodafoejr;ielwjrklesariesr";
+
+function countVowels(str) {
+    const count={}
+    for (let char of str) {
+        if (!count )
+    }
+    for (let char of str) {
+    }
+  }
+
+  function countVowels(testStr) {
+      const count = str.match(/[aeiou]/gi).length
+      return count
+    }
+  
